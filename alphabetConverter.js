1673a5b608ecaf28edf3ebe917f45f0ceb32c6c9
eckolo <eckolo.0722@gmail.com>
2018-04-02 23:27:43 +0900

    イベント定義個所修正

--- alphabetConverter.js
+++ alphabetConverter.js
@@ -1,4 +1,15 @@
 $(function() {
+
+    $("#mainForm").on("click", "#converteButton", function() {
+        $("#outputConverted").html(converteText($("#originString").val()));
+        $("#outputReConverted").html(convertStr($("#outputConverted").val()));
+    });
+
+    $("#mainForm").on("change", "#originString", function() {
+        $("#outputConverted").html(converteText($("#originString").val()));
+        $("#outputReConverted").html(convertStr($("#outputConverted").val()));
+    });
+
     function convertStr(str) {
         var targetChar, lastChar, beforeLastChar;
         var converted;
--- textConverter.js
+++ textConverter.js
@@ -2,17 +2,7 @@ $(function() {
     var consonant = ["", "b", "c", "d", "f", "g", "h", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var vowel = ["a", "e", "i", "o", "u"];
 
-    $("#mainForm").on("click", "#converteButton", function() {
-        $("#outputConverted").html(converteText($("#originString").val()));
-        $("#outputReConverted").html(convertStr($("#outputConverted").val()));
-    });
-
-    $("#mainForm").on("change", "#originString", function() {
-        $("#outputConverted").html(converteText($("#originString").val()));
-        $("#outputReConverted").html(convertStr($("#outputConverted").val()));
-    });
-
-    function converteText(originText) {
+    converteText = (originText) => {
         var outputString = "";
         var onceCode;
         var onceCodeArray;
@@ -21,7 +11,7 @@ $(function() {
         return outputString;
     }
 
-    function extractionChar(originChar) {
+    extractionChar = (originChar) => {
         switch (originChar) {
             case "\r":
             case "\n":
