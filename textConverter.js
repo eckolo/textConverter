b5018ef1ef330c5d2205993dcda052888fa06ca4
eckolo <eckolo.0722@gmail.com>
2018-04-03 06:58:56 +0900

    特殊文字はじき出しロジック修正

--- textConverter.js
+++ textConverter.js
@@ -1,6 +1,7 @@
 $(function() {
     var consonants = ["", "b", "c", "d", "f", "g", "h", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var vowels = ["a", "e", "i", "o", "u"];
+    var specialChars = ["\r", "\n", "\r\n", "!", "?", "！", "？", "「", "」", "…", "、", "。", " ", "　"];
 
     converteText = (originText) => {
         var outputString = "";
@@ -11,28 +12,9 @@ $(function() {
         return outputString;
     }
 
-    extractionChar = (originChar) => {
-        switch (originChar) {
-            case "\r":
-            case "\n":
-            case "\r\n":
-            case "!":
-            case "?":
-            case "！":
-            case "？":
-            case "「":
-            case "」":
-            case "…":
-            case "、":
-            case "。":
-            case " ":
-            case "　":
-                return originChar;
-
-            default:
-                return `${codeToChar(originChar.charCodeAt(0))} `;
-        }
-    }
+    extractionChar = (originChar) => specialChars.includes(originChar) ?
+        originChar :
+        `${codeToChar(originChar.charCodeAt(0))} `;
 
     codeToChar = (onceCode) => {
         var vowel = vowels[onceCode % vowels.length];
