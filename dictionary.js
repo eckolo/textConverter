78ba6e1efaa188bf3446e4640d883424fc38aeea
eckolo <eckolo.0722@gmail.com>
2018-04-05 22:35:50 +0900

    定数部分を切り離し

---
+++ dictionary.js
@@ -0,0 +1,3 @@
+const consonants = ["", "b", "c", "d", "f", "g", "h", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+const vowels = ["a", "e", "i", "o", "u"];
+const specialChars = ["\r", "\n", "\r\n", "!", "?", "！", "？", "「", "」", "…", "、", "。", " ", "　"];
--- textConverter.js
+++ textConverter.js
@@ -1,8 +1,4 @@
 $(function() {
-    const consonants = ["", "b", "c", "d", "f", "g", "h", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-    const vowels = ["a", "e", "i", "o", "u"];
-    const specialChars = ["\r", "\n", "\r\n", "!", "?", "！", "？", "「", "」", "…", "、", "。", " ", "　"];
-
     converteText = (originText) => {
         var outputString = "";
 
